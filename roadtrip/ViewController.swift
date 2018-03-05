e0a19c9bf0967b35ee5443ba7b9fd4fe8f6f2d94
Tokuda, Suguru <stokuda@ilstu.edu>
2018-03-04 18:40:53 -0600

    updated map function

--- roadtrip/ViewController.swift
+++ roadtrip/ViewController.swift
@@ -12,34 +12,25 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestAlwaysAuthorization()
-        
         if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
             locationManager.startUpdatingLocation()
         }
-        
-        currentLocation = locationManager.location
-        lat = currentLocation?.coordinate.latitude
-        long = currentLocation?.coordinate.longitude
-        
-//        if CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedWhenInUse ||
-//            CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedAlways {
-//            currentLocation = locationManager.location
-//            lat = currentLocation?.coordinate.latitude
-//            long = currentLocation?.coordinate.longitude
-//        }
-        
         googleMapsAPIKey = appDelegate.googleMapsAPIKey
         GMSServices.provideAPIKey(googleMapsAPIKey)
-        
-        
-        let camera = GMSCameraPosition.camera(withLatitude: lat!, longitude: long!, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
-        
-        self.view = mapView        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            self.lat = location.coordinate.latitude
+            self.long = location.coordinate.longitude
+            print(location.coordinate)
+            let camera = GMSCameraPosition.camera(withLatitude: lat!, longitude: long!, zoom: 6.0)
+            let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
+            self.view = mapView
+        }
     }
 
     override func didReceiveMemoryWarning() {
