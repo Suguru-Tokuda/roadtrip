1343d8901f0c475d9be02dedee9d1efe82be3a1f
suguru-tokuda <stokuda@ilstu.edu>
2018-03-23 18:08:49 -0500

    Improved car info view and added fuel remaining settings view

--- roadtrip/Car.swift
+++ roadtrip/Car.swift
@@ -8,20 +8,24 @@ public class Car {
     private var _trim: String?
     private var _year: String?
     private var _mileage: Double?
+    private var _fuelCapacity: Double?
     private var _gasType: String?
     private var _mpgHwy: Double?
     private var _mpgCity: Double?
+    private var _fuelRemainingInPercent: Double?
     
     // MARK: Initializer
-    public init(make: String, model: String, trim: String, year: String, mileage: Double, gasType: String, mpgHwy: Double, mpgCity: Double) {
+    public init(make: String, model: String, trim: String, year: String, mileage: Double, fuelCapacity: Double, gasType: String, mpgHwy: Double, mpgCity: Double) {
         self._make = make
         self._model = model
         self._trim = trim
         self._year = year
         self._mileage = mileage
+        self._fuelCapacity = fuelCapacity
         self._gasType = gasType
         self._mpgHwy = mpgHwy
         self._mpgCity = mpgCity
+        self._fuelRemainingInPercent = 100.0
     }
     
     // MARK: Getters & Setters
@@ -70,6 +74,15 @@ public class Car {
         }
     }
     
+    public var fuelCapacity: Double {
+        get {
+            return round(1000 * self._fuelCapacity!) / 1000
+        }
+        set(fuelCapacity) {
+            self._fuelCapacity = fuelCapacity
+        }
+    }
+    
     public var gasType: String {
         get {
             return self._gasType!
@@ -95,6 +108,19 @@ public class Car {
         set(mpgCity) {
             self._mpgCity = mpgCity
         }
+    }
+    
+    public var fuelRemainingInPercent: Double {
+        get {
+            return self._fuelRemainingInPercent!
+        }
+        set(fuelRemainingInPercent) {
+            self._fuelRemainingInPercent = fuelRemainingInPercent
+        }
+    }
+    
+    public func getRemainingGas() -> Double {
+        return round(1000 * self._fuelCapacity! * _fuelRemainingInPercent! / 100) / 1000
     }
     
 }
---
+++ roadtrip/FuelRemainingViewController.swift
@@ -0,0 +1,45 @@
+//
+//  FuelRemainingViewController.swift
+//  roadtrip
+//
+//  Created by Suguru on 3/23/18.
+//  Copyright © 2018 edu.ilstu.stokudasabhat. All rights reserved.
+//
+
+import UIKit
+
+class FuelRemainingViewController: UIViewController {
+    
+    let appDelegate: AppDelegate? = UIApplication.shared.delegate as? AppDelegate
+    var myCar: Car?
+    
+    @IBOutlet weak var fuelRemainingSlider: UISlider!
+    @IBOutlet weak var fuelLabel: UILabel!
+    @IBOutlet weak var percentageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myCar = appDelegate?.myCar
+        fuelRemainingSlider.value = Float(myCar!.fuelRemainingInPercent)
+        percentageLabel.text = "\(fuelRemainingSlider.value.description)%"
+        fuelLabel.text = "\(String(describing: myCar!.getRemainingGas())) / \(String(describing: myCar!.fuelCapacity)) gallons"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func fuelRemainingSliderChanged(_ sender: Any) {
+        myCar?.fuelRemainingInPercent = Double(fuelRemainingSlider.value)
+        let percentage = round(1000 * fuelRemainingSlider.value)/1000
+        percentageLabel.text = "\(percentage.description)%"
+        fuelLabel.text = "\(String(describing: myCar!.getRemainingGas())) / \(String(describing: myCar!.fuelCapacity)) gallons"
+    }
+    
+    @IBAction func ctnBtnClicked(_ sender: Any) {
+        appDelegate?.myCar?.fuelRemainingInPercent = Double(fuelRemainingSlider.value)
+    }
+    
+    
+
+}
